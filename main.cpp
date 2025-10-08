bc8dc81e4b70bdf74bbbdc2f2622f97c64c4f50e
El <404.el.404@gmail.com>
2025-10-08 01:14:42 -0500

    added content

--- main.cpp
+++ main.cpp
@@ -3,97 +3,59 @@
 using namespace Leap;
 
 //compilation command
-//clear;g++ main.cpp -o test -I include -L lib/x64 -l Leap  
+//clear;g++ main.cpp -o chair -I include -L lib/x64 -l Leap  
+//
+//may have to run:
+//export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+
+Hand getHand(Frame frame) {
+    Hand best = frame.hand(0);
+    for (int i = 0; i < frame.hands().count(); i++) {
+        if (!best.isValid() || best.timeVisible() < frame.hand(i).timeVisible()) 
+            best = frame.hand(i);
+    }
+    
+    return best;
+}
 
 int main() {
-    std::string errorMessage = "";
+    bool running = true;
     Controller controller;
     Listener listener;
     Frame recentFrame;
+    HandList hands;
     
     if(!controller.addListener(listener)) {
-        errorMessage = "failed to apply a listener";
-        goto err;
+        std::cout << "failed to apply a listener" << std::endl;
+        controller.removeListener(listener);
+
+        return -1;
     }
 
     std::cout << controller.isConnected() << std::endl;
-
-
-    recentFrame = controller.frame(0);
-
-    return 0;
-
-err:
-    std::cout << errorMessage << std::endl;
-    controller.removeListener(listener);
-
-    return -1;
-}
-
-/*
-LEAP_HAND getHand(LEAP_CONNECTION_MESSAGE msg) {
-    LEAP_HAND mainHand = msg.tracking_event->pHands[0];
-    //default
-
-    for(int i = 0; i < msg.tracking_event->nHands; i++) {
-        if(msg.tracking_event->pHands[i].visible_time > mainHand.visible_time)
-            //if the hand is visible for longer
-            mainHand = msg.tracking_event->pHands[i];
-            //set it to the one to return/use
-    }
-    return mainHand;
-}
-
-
-int main() {
-    LEAP_CONNECTION connection = NULL;
-    eLeapRS result;
-    bool running = true;
-
-    result = LeapCreateConnection(NULL, &connection);
-    if (result != eLeapRS_Success) {
-        printf("Error creating Leap connection\n");
-        running = false;
-    }
-    result = LeapOpenConnection(connection);
-    if (result != eLeapRS_Success) {
-        printf("Error opening Leap connection\n");
-        running = false;
-    }
+    controller.setPolicy(Controller::POLICY_BACKGROUND_FRAMES);
 
 
 
-    //start: main loop
     while (running) {
-        LEAP_CONNECTION_MESSAGE msg;
-        result = LeapPollConnection(connection, 100, &msg);
-        //get data
-        if (result != eLeapRS_Success || msg.type != eLeapEventType_Tracking || msg.tracking_event->nHands <= 0) 
-            continue;
-        //don't calculate if something happened or there are no hands to track
-        LEAP_HAND mainHand = getHand(msg);
-        //end: setup
+        recentFrame = controller.frame(0);
+        hands = recentFrame.hands();
+        std::cout << "num of hands: " << hands.count() << std::endl;
 
 
-
-        double rotation = atan(mainHand.palm.direction.z / mainHand.palm.direction.x ) * 180 / M_PI;
+        Hand hand = getHand(recentFrame);
+        double rotation = atan(hand.direction().z / hand.direction().x ) * 180 / M_PI;
         rotation = rotation < 0 ? rotation + 180 : rotation;
 
-        bool closed = mainHand.grab_strength > 0.25;
+        bool closed = hand.grabStrength() > 0.25;
         
 
         printf("rot: %f --- closed: %d --- yaw: %f\n",
             rotation,
             closed,
-            mainHand.palm.direction.y * 90
+            hand.direction().y * 90
         );
     }
 
-cleanup:
-    // Clean up
-    LeapCloseConnection(connection);
-    LeapDestroyConnection(connection);
-    return 0;
+    controller.removeListener(listener);
 }
-
-*/
